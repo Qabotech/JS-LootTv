e06b62d9c024df6eca8e6611c37e7473ac79130f
Qabotech <81534574+Qabotech@users.noreply.github.com>
2022-06-29 10:44:44 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         loot.tv
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://loot.tv/rewardedtv
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=loot.tv
+// @grant        none
+// ==/UserScript==
+
+
+
+
+setInterval(function() {
+
+let el = document.querySelector(".rewardedtv_playButtonWrapper__JPHsP")
+
+el.click();
+
+}, 5 * 1000);
+
+setInterval(function() {
+  location.reload()
+}, 45 * 1000);
+
